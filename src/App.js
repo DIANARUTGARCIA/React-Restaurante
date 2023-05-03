386af25a123dc533e369790e044056e165f84c2c
Diana Rut <93566502+DIANARUTGARCIA@users.noreply.github.com>
2023-05-03 13:50:48 -0600

    Inicio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-ligth">Hola mundo</h1>
+    <h1>no tiene tailwind :c</h1>
+    </>
+    
+  );
+}
+ export default App;
