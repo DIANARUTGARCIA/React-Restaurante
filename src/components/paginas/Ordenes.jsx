99d69146fd00100e8dd47915ae8687c067626aa6
Diana Rut <93566502+DIANARUTGARCIA@users.noreply.github.com>
2023-05-10 11:54:08 -0600

    Finalizanco Ordenes

--- src/components/paginas/Ordenes.jsx
+++ src/components/paginas/Ordenes.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { FirebaseContext } from '../../firebase'
+import Orden from '../ui/Orden'
 
 const Ordenes = () => {
+  // context con las operaciones de firebase
+  const { firebase } = useContext(FirebaseContext)
+  // state de las ordenes
+  const [ordenes, guardarOrdenes] = useState([])
+
+  useEffect(() => {
+    const obtenerOrdenes = () => {
+      firebase.db
+        .collection('ordenes')
+        .where('completado', '==', false)
+        .onSnapshot(manejarSnapshot)
+    }
+    obtenerOrdenes()
+  }, [])
+
+  function manejarSnapshot(snapshot) {
+    const ordenes = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      }
+    })
+
+    guardarOrdenes(ordenes)
+  }
+
   return (
-    <div>
-      <h1 className="">ORDENESS</h1>
-    </div>
+    <>
+      <h1 className="text-3xl font-light mb-4">Ordenes</h1>
+      <div className="sm:flex sm:flex-wrap -mx-3">
+        {ordenes.map((orden) => (
+          <Orden key={orden.id} orden={orden} />
+        ))}
+      </div>
+    </>
   )
 }
 
